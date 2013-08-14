ab34ad912bac66f9a736b3f52cf44e1056807114
MATSUNO★Tokuhiro <tokuhirom+cpan@gmail.com>
2013-08-14 01:16:26 +0000

    Import of TOKUHIROM/Amon2-Lite-0.10 from CPAN.
    gitpan-cpan-distribution: Amon2-Lite
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         TOKUHIROM/Amon2-Lite-0.10.tar.gz
    gitpan-cpan-author:       TOKUHIROM
    gitpan-cpan-maturity:     released

--- lib/Amon2/Setup/Flavor/Lite.pm
+++ lib/Amon2/Setup/Flavor/Lite.pm
@@ -23,7 +23,7 @@ use lib File::Spec->catdir(dirname(__FILE__), 'extlib', 'lib', 'perl5');
 use lib File::Spec->catdir(dirname(__FILE__), 'lib');
 use Amon2::Lite;
 
-our $VERSION = '0.01';
+our $VERSION = '0.10';
 
 # put your configuration here
 sub load_config {
@@ -46,7 +46,9 @@ get '/' => sub {
 };
 
 # load plugins
-__PACKAGE__->load_plugin('Web::CSRFDefender');
+__PACKAGE__->load_plugin('Web::CSRFDefender' => {
+    post_only => 1,
+});
 # __PACKAGE__->load_plugin('DBI');
 # __PACKAGE__->load_plugin('Web::FillInFormLite');
 # __PACKAGE__->load_plugin('Web::JSON');
@@ -64,9 +66,10 @@ __DATA__
     <meta charset="utf-8">
     <title><% $module %></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script type="text/javascript" src="[% uri_for('/static/js/main.js') %]"></script>
-    <link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css">
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
+    <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="[% uri_for('/static/css/main.css') %]">
 </head>
 <body>
